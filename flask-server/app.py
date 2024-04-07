6b06b88aebf65c72cc51a005eee5f01a2abedfa9
Sasmik23 <mikhilanand@gmail.com>
2024-04-07 23:22:56 +0800

    update db connection

--- flask-server/app.py
+++ flask-server/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
     
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xxsbhhpozcgvyy:e25d18ebd43c02b777cf0413a15aa6f3b38abee6879f0e6801dd8f039c287fc5@ec2-34-193-110-25.compute-1.amazonaws.com:5432/dtpj3qnei78r'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
 app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['CORS_HEADERS'] = 'Content-Type'
 db_init(app)
