bdf396d3aa1392b8edfe286d4d45554a1802c8d2
Sasmik23 <mikhilanand@gmail.com>
2024-04-15 15:01:45 +0800

    update heroku code

--- flask-server/app.py
+++ flask-server/app.py
@@ -16,10 +16,28 @@ app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['CORS_HEADERS'] = 'Content-Type'
 db = SQLAlchemy()
 
+'''
 db.init_app(app)
 with app.app_context():
     db.create_all()
+'''
 
+class images(db.Model):
+    
+    __tablename__ = "images"
+    
+    id = db.Column(db.Integer, primary_key = True)
+    image = db.Column(db.Text,nullable = False)
+    name = db.Column(db.Text, nullable=False)
+    mimetype = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text)
+    
+    def __init__(self,image, name, mimetype, description):
+        self.image = image
+        self.name = name 
+        self.mimetype = mimetype 
+        self.description = description 
+    
 
 @app.route('/')
 def home():
