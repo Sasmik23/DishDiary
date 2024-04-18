3763fb746d55c6f41913ba68c4cb6748904ac175
Sasmik23 <mikhilanand@gmail.com>
2024-04-19 03:30:19 +0800

    test server to run both front and backend

--- flask-server/app.py
+++ flask-server/app.py
@@ -2,7 +2,6 @@ from flask import Flask,jsonify,request, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
-print(os.listdir('/dishdiary/flask-server'))
 from .gpt_manager import generate_image_description
 import base64
 import os
